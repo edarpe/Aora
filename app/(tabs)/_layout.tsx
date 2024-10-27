3abe91cbd60c6c156f9163d33a523e1692a525a3
Edwin Perez <edwin@edarpe.com>
2024-10-27 15:20:11 -0600

    chore: initial config

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import "../../global.css";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
